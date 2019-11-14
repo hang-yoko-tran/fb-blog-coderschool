3fe52fbdb01d93ebc7d6fbd41334b0921672f18f
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-11-14 17:20:33 +0700

    milestone-2-Config Flask App and libraries

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 
 if __name__ == "__main__":
     app.run(debug = True)
