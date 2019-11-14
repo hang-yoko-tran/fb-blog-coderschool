3c1fa8abdfd24748ef43bb99887ceeb74797156c
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-11-14 22:38:57 +0700

    milestone-6-Log-in with correct credentials

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-
+# app.secret_key = 'My secret'
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -34,11 +34,40 @@ class Post(db.Model):
   created_at = db.Column(db.DateTime, server_default=db.func.now()) 
   updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+
+login_manager.login_view='login'
+
+
 db.create_all()
 
 @app.route('/')
 def root():
   return render_template('views/index.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':  # By default (GET REQUEST), python will skip this condition and just return render_template at the end of this function. But If the user submit the form, this line will be checked
+        check_email = User.query.filter_by(email=request.form['email']).first() # we use the email that user provides and check if that email is taken or not
+        if check_email:  #if email taken
+            flash('Email already taken', 'warning') # we alert the user
+            return redirect(url_for('register')) # then reload the register page again
+        # if email not taken, we add new user to the database
+        # we start with create an object for new_user
+        new_user = User(name=request.form['name'],  
+                        email=request.form['email'])
+        new_user.generate_password(request.form['password'])  # raw password will be hashed using the generate_password method
+        db.session.add(new_user) # then we add new user to our session
+        db.session.commit() # then we commit to our database (kind of like save to db)
+        login_user(new_user) # then we log this user into our system
+        flash('Successfully create an account and logged in', 'success')
+        return redirect(url_for('root')) # and redirect user to our root
+    return render_template('views/register.html')
 
 
 if __name__ == "__main__":
