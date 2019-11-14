d8134a43d770dae64a032cb1fb79cfe23becec47
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-11-14 22:15:51 +0700

    milestone-4-Create a route with a view

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+    def generate_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Post(db.Model):
+  __tablename__ = 'posts'
+  id = db.Column(db.Integer, primary_key=True)
+  body = db.Column(db.String, nullable=False)
+  user_id = db.Column(db.Integer, nullable=False)
+  created_at = db.Column(db.DateTime, server_default=db.func.now()) 
+  updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+db.create_all()
+
+
+
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
